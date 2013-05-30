0953e7ae55ed037cd91a6d7ef094c4b88d8a1a62
Jeff <jeff.smits@gmail.com>
2013-05-30 12:07:10 +0200

    Added redundant Stm constructor to fix pretty printer. Completed transformations for clean-sql. See test-1.sql for all effects of the transformation.

--- syntax/SQL.pp
+++ syntax/SQL.pp
@@ -1,11 +1,17 @@
 %% Pretty printing table (see also SQL.generated.pp)
 [
-   Stms                                  -- V[ _1 ] KW[";"],
-   Stms.1:iter-sep                       -- H[ _1 KW[";"] ],
+   Stms                                  -- V[ _1 ],
+   Stms.1:iter                           -- _1,
+   Stm                                   -- H hs=0[ H[ _1 ] KW[";"] ],
+   
    From                                  -- KW["FROM"] _1,
+   
    OrderBy                               -- KW["ORDER"] KW["BY"] _1,
+   
    ValuesClause                          -- KW["VALUES"] _1,
    ValuesClause                          -- KW["VALUE"] _1,
+   
    OnDuplicateKeyClause                  -- KW["ON"] KW["DUPLICATE"] KW["KEYS"] KW["UPDATE"] _1,
+   
    SetClause                             -- KW["SET"] _1
 ]
--- test/test-1.sql
+++ test/test-1.sql
@@ -1,9 +1,9 @@
-SELECT * FROM afdeling;
+SELECT * FROM afdeling1;
 
 CREATE VIEW view1 AS SELECT * FROM afdeling;
 
-SELECT * FROM afdeling1;
+SELECT * FROM view1;
 
 DROP VIEW view1;
 
-SELECT * FROM afdeling2;
+SELECT * FROM afdeling3;
