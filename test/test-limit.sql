4d6dca7e6ed975f5a23f96617400cce300f769fd
Jeff <jeff.smits@gmail.com>
2013-06-04 15:19:53 +0200

    Improved limit test (removed view, added comments and non-limited select). Added transformation to always limit things up to 50 if no stricter limit is present.

--- test/test-limit.sql
+++ test/test-limit.sql
@@ -1,7 +1,14 @@
+-- unlimited
+SELECT * FROM afdeling;
+-- ok limit
 SELECT * FROM afdeling LIMIT 40;
+-- too high limit
 SELECT * FROM afdeling LIMIT 60;
-CREATE VIEW a AS SELECT * FROM afdeling;
+-- only just ok limit
 SELECT * FROM afdeling LIMIT 5,50;
+-- too high limit
 SELECT * FROM afdeling LIMIT 5,100;
+-- too high limit in different syntax
 SELECT * FROM afdeling LIMIT 60 OFFSET 100;
+-- ok limit in different syntax
 SELECT * FROM afdeling LIMIT 45 OFFSET 100;
