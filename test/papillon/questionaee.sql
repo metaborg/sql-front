839b949d0c9c1fe8dd5e4b89c018eca1b87e86da
Jeff <jeff.smits@gmail.com>
2013-06-11 12:36:30 +0200

    Changed some more functions to uppercase.

--- test/papillon/questionaee.sql
+++ test/papillon/questionaee.sql
@@ -1,1 +1,5 @@
-      CREATE   VIEW hulp1 (klant, bedrag) AS          SELECT   klant, AVG(bedrag)          FROM   verkoop          GROUP   BY klant;           CREATE   VIEW hulp2 (klant, datum) AS          SELECT   klant, datum          FROM   verkoop          WHERE   klant IN (   SELECT   klant                FROM   hulp1                WHERE   bedrag = (   SELECT   MAX(bedrag)                      FROM   hulp1));           SELECT   klant, datum       FROM   hulp2       WHERE   datum = (   SELECT   MAX(datum)             FROM   hulp2);           DROP VIEW hulp2;       DROP VIEW hulp1;       
+      CREATE   VIEW hulp1 (klant, bedrag) AS          SELECT   klant, AVG(bedrag)          FROM   verkoop          GROUP   BY klant;
+      CREATE   VIEW hulp2 (klant, datum) AS          SELECT   klant, datum          FROM   verkoop          WHERE   klant IN (   SELECT   klant                FROM   hulp1                WHERE   bedrag = (   SELECT   MAX(bedrag)                      FROM   hulp1));
+      SELECT   klant, datum       FROM   hulp2       WHERE   datum = (   SELECT   MAX(datum)             FROM   hulp2);
+      DROP VIEW hulp2;
+      DROP VIEW hulp1;       
