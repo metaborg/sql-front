8c7a5932653e5692b025696e976ee908b16ca92c
Jeff <jeff.smits@gmail.com>
2013-06-11 10:53:07 +0200

    Added rules to print parentheses of a function after the function.

--- syntax/SQL.pp
+++ syntax/SQL.pp
@@ -15,6 +15,18 @@
    
    SetClause                             -- KW["SET"] _1,
    
+   ExtractFun                      -- H hs=0[ H[ _1 ] "(" H[ _2 ] ")" ],
+   Case                            -- H hs=0[ KW["CAST"] "(" H[ _1 KW["AS"] _2 ] ")" ],
+   NullIf                          -- H hs=0[ KW["NULLIF"] "(" H[ _1 ] "," H[ _2 ] ")"],
+   Coalesce                        -- H hs=0[ KW["COALESCE"] "(" H[ _1 ] ")"],
+   Aggr                            -- H hs=0[ H[ _1 ] "(" H[ _2 _3 ] ")" ],
+   Substring                       -- H hs=0[ KW["SUBSTRING"] "(" H[ _1 KW["FROM"] _2 _3 ] ")" ],
+   Upper                           -- H hs=0[ KW["UPPER"] "(" H[ _1 ] ")"],
+   Lower                           -- H hs=0[ KW["LOWER"] "(" H[ _1 ] ")"],
+   Convert                         -- H hs=0[ KW["CONVERT"]   "(" H[ _1 KW["USING"] _2 ] ")"],
+   Translate                       -- H hs=0[ KW["TRANSLATE"] "(" H[ _1 KW["USING"] _2 ] ")"],
+   Trim                            -- H hs=0[ KW["TRIM"] "(" H[ _1 _2 ] ")"],
+   
    amb                                   -- KW["!ambiguous!"] "(" V[ _1 ] ")",
    amb.1:iter-star                       -- H[ _1 ] 
 ]
